8d7875204a629886d7cc91623689fc6224b84604
ArtemKhairov <artemxairoff@yandex.ru>
2023-12-02 21:57:22 +0700

    add refresh method

--- src/widgets/users-list/ui/index.tsx
+++ src/widgets/users-list/ui/index.tsx
@@ -1,10 +1,41 @@
 import { observer } from "mobx-react-lite";
 import { userSessionModel } from "@/features/user-session";
+//
+import { Button, List, Skeleton, Typography } from "antd";
+import { FC } from "react";
+
+interface IProps {
+  action: () => void;
+}
+
+const ListHeader: FC<IProps> = ({ action }) => {
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Typography style={{ marginRight: "15px" }}>Пользователи</Typography>
+      <Button onClick={action}>Обновить</Button>
+    </div>
+  );
+};
 
 const UsersList = observer(() => {
-  const { users } = userSessionModel.useUserListStore();
-  console.log(users,"Users Component");
-  return <div>1</div>;
+  const { users, load } = userSessionModel.useUserListStore();
+
+  if (users.length <= 0) {
+    return <Skeleton />;
+  }
+
+  return (
+    <List
+      header={<ListHeader action={load} />}
+      bordered
+      dataSource={users}
+      renderItem={(item) => (
+        <List.Item>
+          <Typography>{item.first_name}</Typography>
+        </List.Item>
+      )}
+    />
+  );
 });
 
 export { UsersList };
